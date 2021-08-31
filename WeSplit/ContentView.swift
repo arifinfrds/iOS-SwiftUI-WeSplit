617a3e554e4c479fef8617af73eed26d1a857b8e
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-08-31 14:47:30 +0700

    Domenstrate `ForEach` loop to show 30 Forms

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
     
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            ForEach(0..<30) { number in
+                Text("Hi \(number)")
+            }
         }
     }
 }
