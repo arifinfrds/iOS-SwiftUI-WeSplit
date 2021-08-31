91bf550c04774379295fdb89d613810f5394c5ac
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-08-31 14:14:29 +0700

    Add `NavigationView` with title (Equivalent to UIKit's UINavigationBar(

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+                Section {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
             }
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
+            .navigationBarTitle(Text("SwiftUI"))
         }
     }
 }
