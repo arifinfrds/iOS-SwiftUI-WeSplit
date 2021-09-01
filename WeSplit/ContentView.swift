14555fce44283d94131c2b4671c0efa9d092154a
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-01 11:26:43 +0700

    reset the view

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let students = ["Harry", "Hermoine", "Ron"]
-    @State private var selectedStudentIndex = 0
     
     var body: some View {
-        VStack {
-            Picker("Select Students : ", selection: $selectedStudentIndex) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
-                }
-            }
-            Text("You chose: Student # \(students[selectedStudentIndex])")
-        }
+        Text("Hello World")
     }
 }
 
